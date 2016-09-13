349a8cbe78f735efd009b93545606941b2c646d5
Stanley Pan <stanleypan.egre@gmail.com>
2016-09-13 16:30:09 +0900

    Add helper extension for UIView

--- Intro Guide/ViewController.swift
+++ Intro Guide/ViewController.swift
@@ -9,9 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let guideCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = UIColor.redColor()
+        
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(guideCollectionView)
+        
+    }
+}
+
+extension UIView {
+    
+    func anchorToTop(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil) {
+        
+        anchorWithConstantsToTop(top, left: left, bottom: bottom, right: right, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0)
+    }
+    
+    func anchorWithConstantsToTop(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, topConstant: CGFloat = 0, leftConstant: CGFloat = 0, bottomConstant: CGFloat = 0, rightConstant: CGFloat = 0) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraintEqualToAnchor(top, constant: topConstant).active = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraintEqualToAnchor(bottom, constant: -bottomConstant).active = true
+        }
+        
+        if let left = left {
+            leftAnchor.constraintEqualToAnchor(left, constant: leftConstant).active = true
+        }
+        
+        if let right = right {
+            rightAnchor.constraintEqualToAnchor(right, constant: -rightConstant).active = true
+        }
     }
 }
